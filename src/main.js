c7d19948ddfc320e6f933c796aaf7b9b2006a71c
ilarioforcherio <ilarioforcherio@gmail.com>
2022-12-02 13:07:38 +0100

    creating project

---
+++ src/main.js
@@ -0,0 +1,29 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+  /* import the fontawesome core */
+  import { library } from '@fortawesome/fontawesome-svg-core'
+
+  /* import font awesome icon component */
+  import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+  /* import specific icons */
+  import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+  /*Import icone regular */
+  import { faFaceSmile } from '@fortawesome/free-regular-svg-icons'
+
+  /* add icons to the library */
+  library.add(faUserSecret,faFaceSmile)
+
+// https://github.com/sylar9696/live-coding-cl-77/tree/main/37-vue-cli-axios
+
+//ATTENZIONE: se la stessa icona viene importata con due stili diversi ad esempio: regular e solid, non possiamo usare lo stesso nome per le due icone ma dobbiamo //usare un rieticchetamento con "as", il codice diverrà:
+
+  /* add font awesome icon component */
+  Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
