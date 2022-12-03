3c891d24b61372e8b7313423d33c6c37ba15a40e
ilarioforcherio <ilarioforcherio@gmail.com>
2022-12-03 15:23:36 +0100

    sec8

--- src/components/Section8Comp.vue
+++ src/components/Section8Comp.vue
@@ -1,32 +1,65 @@
 <template>
-    <div>
-section 8
+    <div class="section8">
+        <div class="sec8Content">
+            <div class="sec8Text">
+                <h2>text</h2> 
+            </div>
+            <div class="sec8Img">
+               
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
-     export default {
-        name:'Section8Comp',
-        components:{
+export default {
+    name: 'Section8Comp',
+    components: {
 
-        },
-        props:{
+    },
+    props: {
 
-        },
-        data(){
-            return{
+    },
+    data() {
+        return {
 
-            }
-        },
-        mounted(){
-
-        },
-        methods:{
-            
         }
+    },
+    mounted() {
+
+    },
+    methods: {
+
     }
+}
 </script>
 
 <style lang="scss" scoped>
+.section8 {
+    .sec8Content {
+        display: flex;
 
+         .sec8Text {
+            flex-basis: 50%;
+            h2 {
+                text-align: center;
+            }
+        }
+        .sec8Img {
+            height: 90vh;
+            flex-basis: 50%;
+            background-image: url('../assets/img/h3-img-5a.jpg');
+            background-position: center;
+            background-repeat: no-repeat;
+            background-size: cover;
+            display: flex;
+            justify-content: center;
+            img {
+                padding: 50px;
+                width: 80%;
+            }
+        }
+       
+    }
+}
 </style>
