00ba600af89f98fd89abb2569e6f5cb1fd0a77d2
ilarioforcherio <ilarioforcherio@gmail.com>
2022-12-03 13:38:45 +0100

    sections

---
+++ src/components/Section2Comp.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="section2">
+    
+
+    </div>
+</template>
+
+<script>
+     export default {
+        name:'Section2Comp',
+        components:{
+
+        },
+        props:{
+
+        },
+        data(){
+            return{
+
+            }
+        },
+        mounted(){
+
+        },
+        methods:{
+            
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+.section2{
+    height: 30vh;
+    background-image: url('../assets/img/h3-testimonials-bckgrnd.jpg');
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover; 
+}
+
+</style>
